e7c9c39666b32a10df4313fc5acd42203a9b6313
Ellie <elliemmanifold@gmail.com>
2019-11-22 11:03:19 +0000

    Refactor tests

--- spec/Note_spec.rb
+++ spec/Note_spec.rb
@@ -2,10 +2,10 @@ require 'Note'
 
 describe Note do
   describe '#display' do
-    let (:formatter_double) { double :formatter, format: "Title: Note title: Note body" }
     let(:note) { Note.new('x', 'z', formatter_double) }
+    let (:formatter_double) { double :formatter, format: "Title: x: z" }
     it "formats note and displays result" do
-      expect(note.display).to eq "Title: Note title: Note body"
+      expect(note.display).to eq "Title: #{note.title}: #{note.body}"
     end
   end
 end
@@ -14,7 +14,7 @@ describe NoteFormatter do
   describe '#format' do
     let(:note) { double :note, title: 'Note title', body: 'Note body' }
     it "returns a sentence" do
-      expect(subject.format(note)).to eq "Title: Note title: Note body"
+      expect(subject.format(note)).to eq "Title: #{note.title}: #{note.body}"
     end
   end
 end
